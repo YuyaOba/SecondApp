dfe34e8fa7679e1f2b6edd0331a4e012ba20e0a9
Yuya Oba <yuyaoba.0501@gmail.com>
2020-11-12 19:45:42 +0900

    first commit

---
+++ SecondApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SecondApp
+//
+//  Created by 大場勇弥 on 2020/11/12.
+//  Copyright © 2020 大場勇弥. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
